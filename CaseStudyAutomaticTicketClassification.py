d20ba8fafff0f935920b75c799adad076b2534c0
Eeshan Gupta <eeshan.gupta@hotmail.com>
2024-02-25 20:57:44 +0000

    Om Ganeshay Namah.

---
+++ CaseStudyAutomaticTicketClassification.py
@@ -0,0 +1,47 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Automatic Ticket Classification
+# Eeshan Gupta  
+# eeshangpt@gmail.com
+
+# %% [markdown]
+# ## Introduction to Problem Statement
+
+# %% [markdown]
+# For a financial company, customer complaints carry a lot of importance, as they are often an indicator of the shortcomings in their products and services. If these complaints are resolved efficiently in time, they can bring down customer dissatisfaction to a minimum and retain them with stronger loyalty. This also gives them an idea of how to continuously improve their services to attract more customers.
+
+# %% [markdown]
+# ### Business goal
+#
+# You need to build a model that is able to classify customer complaints based on the products/services. By doing so, you can segregate these tickets into their relevant categories and, therefore, help in the quick resolution of the issue.
+
+# %% [markdown]
+# ## Table of content
+#
+# 1. [Introduction to problem statemtent](#Introduction-to-Problem-Statement)
+# 2. [Reading in the data](#)
+# 3. [Cleaning the data](#)
+# 4. [Pre-processing the data](#)
+# 5. [Data Visualization](#)
+# 6. [Feature Engineering](#)
+# 7. [Model Building](#)
+# 8. [Inferences from the model](#)
+
+# %% [markdown]
+# ## Reading the data
+#
+# ### Installations and Imports
